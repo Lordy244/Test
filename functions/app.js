0664d6c01a8bc72f2fc2b3148940a21159dc7b8e
Lordy244 <luis.garcia@dgmxtech.com>
2024-08-14 11:55:10 -0600

    gg

---
+++ functions/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const serverless = require('serverless-http');
+const app = express();
+const router = express.Router();
+
+app.use(cors());
+// Define a basic endpoint
+
+router.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+router.get('/about', (req, res) => {
+  res.send('This is the about page.');
+});
+
+app.use('/.netlify/functions/app', router);
+
+module.exports.handler = serverless(app);
