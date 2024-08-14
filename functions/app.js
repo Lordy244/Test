3d9017561e5e6663b406c91d75bf5580ff2c4bfa
Lordy244 <luis.garcia@dgmxtech.com>
2024-08-14 13:49:58 -0600

    sd

--- functions/app.js
+++ functions/app.js
@@ -12,6 +12,30 @@ router.get('/', (req, res) => {
     res.send('Hello, World!!');
 });
 
+router.get('/download-file', async (req, res) => {
+    const fileId = 'dswt0e05bb00cef80425bae6e1efc86d84ba1'; // Replace with the URL of the file you want to download
+  
+    try {
+      // Download the file using Axios
+      const response = await axios.get(`https://download-accl.zoho.com/v1/workdrive/download/${fileId}`, {
+        headers: { 
+            'Authorization': 'Zoho-oauthtoken 1000.f02a139267794858cc3564527f33987c.f7ddc1836fc32c6e9c80214ba348b5fc'
+        },
+        responseType: 'arraybuffer', // Important to handle binary data correctly
+      });
+  
+      // Set the appropriate headers for the response
+      res.setHeader('Content-Disposition', 'attachment; filename="downloaded-file.jpg"'); // Adjust filename and type as necessary
+      res.setHeader('Content-Type', response.headers['content-type']);
+  
+      // Send the file content in the response
+      res.send(response.data);
+    } catch (error) {
+      console.error('Error downloading the file:', error);
+      res.status(500).send('Failed to download file');
+    }
+});
+
 router.get('/about', (req, res) => {
   res.send('This is the about page.');
 });
